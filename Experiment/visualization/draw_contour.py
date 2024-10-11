c8b3f7d09eb0d6a7a45bfc6810c55b8af31f5beb
Chloewz <1291118617@qq.com>
2024-10-11 11:13:58 +0800

    登高线图绘制代码完善

--- Experiment/visualization/draw_contour.py
+++ Experiment/visualization/draw_contour.py
@@ -2,17 +2,31 @@
 # ! 等高线图的绘制
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.cm as cm
 
 c=9
 
-x = np.linspace(0,1,10000)
-y = np.linspace(0,1,10000)
+x = np.linspace(0,1,500)  # epison
+y = np.linspace(0,1,500)  # p
 X, Y = np.meshgrid(x,y)
-Z = (Y * (c - 1 + 2 * X) + X) / (c - 1)
+# Z = 1 - Y + X * (2* Y-1) # 分对的
+Z = Y+X*(1-2*Y)/(c-1) # 分错的
+L = -(1-Z)**2*np.log(Z)
 
-fig, ax = plt.subplots()
-CS = ax.contour(X, Y, Z)
-ax.clabel(CS)
-ax.set_title('A Simple')
+plt.figure(figsize=(8,6))
+contour = plt.contour(X, Y, L, levels=20,cmap='coolwarm')
+
+cbar=plt.colorbar(contour)
+cbar.set_label('L value')
+plt.clabel(contour, inline=True, fontsize=8)
+
+plt.xlim(0,0.2)
+plt.ylim(0,0.2)
+
+plt.title('false loss')
+plt.xlabel('epison', fontsize=12)
+plt.ylabel('p', fontsize=12)
+
+plt.tight_layout()
 plt.show()
